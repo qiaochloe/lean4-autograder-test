07cfac6f432affdecd5c29cb30a28204579a72a6
Chloe Qiao <qiaochloe@gmail.com>
2024-08-05 15:13:09 -0400

    Update Lean and dependencies to Lean 4.10

--- lakefile.lean
+++ lakefile.lean
@@ -17,4 +17,3 @@ lean_lib LoVe {
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "ba80034619884735df10ebb449b4fc2a44b2c3e7"
 require autograder from git "https://github.com/robertylewis/lean4-autograder-main" @ "306fd844e8bdcd6c7440ed9b7ae7f16ca86c10cd"
-  
